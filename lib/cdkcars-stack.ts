c9a3839dfb8508f0c45858bedb26393bb524de21
MiguelSan0101 <108699988+MiguelSan0101@users.noreply.github.com>
2022-07-21 15:48:49 -0500

    commit

--- lib/cdkcars-stack.ts
+++ lib/cdkcars-stack.ts
@@ -2,14 +2,11 @@ import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
 
-/**
- * Github Code here
- */
 export class CdkcarsStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const pipeline = new CodePipeline(this, 'Pipeline', {
+    new CodePipeline(this, 'Pipeline', {
         pipelineName: 'TestPipeline',
         synth: new ShellStep('Synth',{
           input: CodePipelineSource.gitHub('MiguelSan0101/CDKCARS','main'),
