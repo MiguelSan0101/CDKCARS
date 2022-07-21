104103cbf85029153b749cd198b3600367274438
MiguelSan0101 <108699988+MiguelSan0101@users.noreply.github.com>
2022-07-21 16:21:02 -0500

    init lambda

--- lib/cdkcars-stack.ts
+++ lib/cdkcars-stack.ts
@@ -1,12 +1,13 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
+import { CodePipeline, CodePipelineSource, ManualApprovalStep, ShellStep } from 'aws-cdk-lib/pipelines';
+import { MyPipelineAppStage } from './stage';
 
 export class CdkcarsStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    new CodePipeline(this, 'Pipeline', {
+    const pipeline = new CodePipeline(this, 'Pipeline', {
         pipelineName: 'TestPipeline',
         synth: new ShellStep('Synth',{
           input: CodePipelineSource.gitHub('MiguelSan0101/CDKCARS','main'),
@@ -15,6 +16,18 @@ export class CdkcarsStack extends Stack {
                      'npx cdk synth']
         }),
       });
+
+      const testingStage = pipeline.addStage(new MyPipelineAppStage(this, "test", {
+        env: { account: "591039258532", region: "us-east-2" }
+      }));
+  
+  
+      testingStage.addPre(new ShellStep("Run Unit Tests", { commands: ['npm install', 'npm test'] }));
+      testingStage.addPost(new ManualApprovalStep('Manual approval before production'));
+  
+      const prodStage = pipeline.addStage(new MyPipelineAppStage(this, "prod", {
+        env: { account: "591039258532", region: "us-east-2" }
+      }));
 
   }
 }
