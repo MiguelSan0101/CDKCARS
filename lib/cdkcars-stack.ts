27d148205d5c0f1a13d9db459d9fbc6bc074f071
MiguelSan0101 <108699988+MiguelSan0101@users.noreply.github.com>
2022-07-21 13:10:21 -0500

    2commit

--- lib/cdkcars-stack.ts
+++ lib/cdkcars-stack.ts
@@ -1,20 +1,46 @@
-import { RemovalPolicy, Stack, StackProps } from 'aws-cdk-lib';
-import { AttributeType, Table } from 'aws-cdk-lib/aws-dynamodb';
+import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as pipelines from "aws-cdk-lib/pipelines";
 
+/**
+ * Github Code here
+ */
 export class CdkcarsStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    new Table (this,'carros',{
-      partitionKey:{
-        name:'id',
-        type:AttributeType.STRING
-      },
-      tableName:'carros',
-      removalPolicy:RemovalPolicy.DESTROY,
-    })
+    const pipeline = new pipelines.CodePipeline(this, 'Pipeline', {
+      // The pipeline name
+      pipelineName: 'CdkCarsPipeline',
+
+      // How it will be built and synthesized
+      synth: new pipelines.CodeBuildStep('SynthStep', {
+          input: pipelines.CodePipelineSource.connection('MiguelSan0101/CDKCARS', 'main', {
+              connectionArn: 'arn:aws:codestar-connections:us-east-2:591039258532:connection/731acae5-6c4d-43f6-aef5-2d333457312a'
+          }),
+          installCommands: [
+            'npm install -g aws-cdk'    
+          ],
+          commands: [
+            'npm ci',
+            'npm run build',
+            'npx cdk synth'
+          ],
+      })
+    });
     
+    // This is where we add the application stages
+    // ...
+  
   }
 }
+
+    // new CodePipeline(this, 'Pipeline', {
+    //   pipelineName: 'TestPipeline',
+    //   synth: new ShellStep('Synth',{
+    //     input: CodePipelineSource.gitHub('MiguelSan0101/CDKCARS','main'),
+    //     commands: ['npm ci',
+    //                'npm run build',
+    //                'npx cdk synth']
+    //   }),
+    // });
