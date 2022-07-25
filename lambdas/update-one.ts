15e87553ad115000809422491af6670d61975450
MiguelSan0101 <108699988+MiguelSan0101@users.noreply.github.com>
2022-07-25 16:40:31 -0500

    update

--- lambdas/update-one.ts
+++ lambdas/update-one.ts
@@ -10,7 +10,7 @@ const RESERVED_RESPONSE = `Error: You're using AWS reserved keywords as attribut
 const db = new AWS.DynamoDB.DocumentClient();
 
 export const handler = async (event: any = {}): Promise<any> => {
-  const {modelo, marca, ...resto } = JSON.parse(event.body);
+  const {marca, ...resto } = JSON.parse(event.body);
   if (!event.body) {
     return { statusCode: 400, body: 'solicitud no válida, falta el cuerpo del parámetro' };
   }
@@ -23,19 +23,18 @@ export const handler = async (event: any = {}): Promise<any> => {
   }
 
   const editedItem: any = typeof event.body == 'object' ? event.body : JSON.parse(event.body);
-  const editedItemProperties = Object.keys(editedItem);
-  if (!editedItem || editedItemProperties.length < 1) {
+  
+  if (!editedItem) {
     return { statusCode: 400, body: 'solicitud inválida, no se proporcionaron argumentos' };
   }
   var m = editedItemId.split("_",2)
-  const firstProperty = editedItemProperties.splice(0, 1);
   const params: any = {
     TableName: TABLE_NAME,
     Key: {
       marca: m[0], //Primary key
       modelo:m[1]  //Sort key
     },
-    UpdateExpression: `set ${firstProperty} = :${firstProperty}`,
+    UpdateExpression: `set marca = :${marca}`,
     ExpressionAttributeValues: {},
     ReturnValues: 'UPDATED_NEW'
   }
