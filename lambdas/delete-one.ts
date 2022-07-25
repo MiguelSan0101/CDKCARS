61f4f5a4081350a404409289858b7985cdbc855e
MiguelSan0101 <108699988+MiguelSan0101@users.noreply.github.com>
2022-07-25 15:28:25 -0500

    delete

--- lambdas/delete-one.ts
+++ lambdas/delete-one.ts
@@ -23,10 +23,10 @@ export const handler = async (event: any = {}): Promise<any> => {
   }
 
   try {
-    // const statement = `SELECT * FROM ${TABLE_NAME} WHERE marca = '${marca[0]}' and modelo = '${marca[1]}' `
-    // const results = await ddb.executeStatement({Statement: statement}).promise();
+    const statement = `SELECT * FROM ${TABLE_NAME} WHERE marca = '${marca[0]}' and modelo = '${marca[1]}' `
+    const results = await ddb.executeStatement({Statement: statement}).promise();
 
-    await db.delete(params).promise();
+    //await db.delete(params).promise();
     return { statusCode: 200, body: 'Exito al eliminar item' };
   } catch (dbError) {
     return { statusCode: 500, body: JSON.stringify(dbError)+`M0: ${marca[0]} M1: ${marca[1]}` };
